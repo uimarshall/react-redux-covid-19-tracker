7883efac96887b57324908c7617c0c5e6351f985
uimarshall <uimarshall@gmail.com>
2021-03-01 23:13:59 +0100

    create react app and remove unwanted files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>covid app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
