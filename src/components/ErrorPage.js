3fe0e7a2a8a0bb0b8bf28415363a1a9091e3a39b
uimarshall <uimarshall@gmail.com>
2021-03-02 01:03:07 +0100

    Add hemo page and ErrorPage with stylings

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import errorImg from '../assets/images/searching.svg';
+import '../assets/css/ErrorPage.css';
+
+function ErrorPage() {
+  return (
+    <div className="error-page">
+      <div className="container">
+        <div className="error-img"><img src={errorImg} alt="error" /></div>
+        <h1>404</h1>
+        <p>sorry, the page you are looking for cannot be found</p>
+        <Link to="/" className="btn">back home</Link>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default ErrorPage;
